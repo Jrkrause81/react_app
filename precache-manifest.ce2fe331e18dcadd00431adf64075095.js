5b48179e34292be3a639d03bc775c19b55a954b5
JBone Krause <jrkrause81@gmail.com>
2020-07-13 14:45:22 -0500

    Updates

---
+++ precache-manifest.ce2fe331e18dcadd00431adf64075095.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992b04a153bcfac0039fc74692ea422c",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "d4cdc4c52507dcd02dec",
+    "url": "/react_app/static/css/main.6ac42d63.chunk.css"
+  },
+  {
+    "revision": "40a536335e184874c60b",
+    "url": "/react_app/static/js/2.ff10b991.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_app/static/js/2.ff10b991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4cdc4c52507dcd02dec",
+    "url": "/react_app/static/js/main.3eda6ef9.chunk.js"
+  },
+  {
+    "revision": "8443a1a7bdd95db3dda3",
+    "url": "/react_app/static/js/runtime-main.f8c04993.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/react_app/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/react_app/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
